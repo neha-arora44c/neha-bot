3bb5e9ec155dedcf18d196da327d15a8f8257c4d
neha-arora44c <neha.arora44c@gmail.com>
2016-04-22 22:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'verify_neha_bot') {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -28,3 +28,40 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "CAAWwHGmQ1ocBACC1QBZC9A3fQlmoSKqZCZCs1bjZC5OZAV7WBu5qsMWORokDZAaFnGwkQ1kAjq8yZAfYWiXRx8qnQGDgeCS39OYKppne3tICHmWIxOZBFoCkfNnCfFhFLwyFZCNNB3WEJb6YtBos4kELcFdKnWPWS4EJ6bym9VgNrpmugm50jmk3SkxQ0SZBQxpi8ZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
